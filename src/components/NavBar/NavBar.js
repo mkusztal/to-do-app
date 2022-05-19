18afb8943011ee4b4c9a962b4aabad6c38d21e99
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-05-19 18:56:28 +0300

    Change Link for NavLink, add colour for links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,5 @@
 import styles from './NavBar.module.scss';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
@@ -7,13 +8,34 @@ const NavBar = () => {
         <i className="fa fa-rocket"></i>
         <ul className={styles.navMenu}>
           <li>
-            <a href="/">Home</a>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? styles.linkActive : undefined
+              }
+              to="/"
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <a href="/favorite">Favorite</a>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? styles.linkActive : undefined
+              }
+              to="/favorite"
+            >
+              Favorite
+            </NavLink>
           </li>
           <li>
-            <a href="/about">About</a>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? styles.linkActive : undefined
+              }
+              to="/about"
+            >
+              About
+            </NavLink>
           </li>
         </ul>
       </div>
